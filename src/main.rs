445191d2fab1d51492e1c1d297ad9afbb515ab51
connor <connormullett@gmail.com>
2021-04-24 01:09:23 -0400

    can finally read tls

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-extern crate pem;
-extern crate rustls;
+use native_tls::{Identity, TlsAcceptor, TlsStream};
+use std::fs::File;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+use std::sync::Arc;
+use std::thread;
 
 #[macro_use]
 extern crate log;
@@ -7,80 +11,38 @@ extern crate log;
 use env_logger;
 use log::Level;
 
-use std::{
-    fs::File,
-    io::{self, BufReader, Read, Write},
-    net::{SocketAddr, TcpListener},
-    path::Path,
-    sync::Arc,
-};
-
-use rustls::{internal::pemfile::certs, Certificate, NoClientAuth, PrivateKey, RootCertStore};
-
-fn load_certs(path: &Path) -> io::Result<Vec<Certificate>> {
-    certs(&mut BufReader::new(File::open(path)?))
-        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid cert"))
-}
-
-fn load_key(path: &Path) -> io::Result<PrivateKey> {
-    let mut reader = BufReader::new(File::open(path)?);
-    let mut data = String::new();
-    let _ = reader.read_to_string(&mut data)?;
-    let pem = pem::parse(data);
-
-    Ok(PrivateKey(pem.unwrap().contents))
-}
-
-fn make_config() -> Arc<rustls::ServerConfig> {
-    let roots = load_certs(Path::new("./cert.pem")).unwrap();
-    let mut client_auth_roots = RootCertStore::empty();
-    for root in &roots {
-        client_auth_roots.add(&root).unwrap();
-    }
-
-    let client_auth = NoClientAuth::new();
-
-    let mut config = rustls::ServerConfig::new(client_auth);
-
-    config.set_persistence(rustls::ServerSessionMemoryCache::new(256));
-
-    let private_key = load_key(Path::new("./key.pem")).unwrap();
-
-    config.set_single_cert(roots, private_key).unwrap();
-
-    Arc::new(config)
+fn handle_client(stream: &mut TlsStream<TcpStream>) {
+    let mut buf = Vec::new();
+    let _ = stream.read_to_end(&mut buf).unwrap();
+    let buf = String::from_utf8_lossy(&buf);
+    log!(Level::Info, "data :: {}", buf);
 }
 
 fn main() {
-    let port = 1965;
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    env_logger::Builder::new().parse_filters("info").init();
 
-    env_logger::Builder::new().parse_filters("trace").init();
+    let mut file = File::open("localhost.pfx").unwrap();
+    let mut identity = vec![];
 
-    let config = make_config();
+    file.read_to_end(&mut identity).unwrap();
+    let identity = Identity::from_pkcs12(&identity, "hunter2").unwrap();
 
-    let listener = TcpListener::bind(addr).expect("cant listen on port");
-    log!(Level::Info, "listening on port {}", 1965);
+    let listener = TcpListener::bind("0.0.0.0:1965").unwrap();
+    let acceptor = TlsAcceptor::new(identity).unwrap();
+    let acceptor = Arc::new(acceptor);
 
-    loop {
-        match listener.accept() {
-            Ok((mut socket, addr)) => {
-                log!(Level::Info, "Accepting new connection from {:?}", addr);
-                let mut tls_session = rustls::ServerSession::new(&config);
-
-                let mut buf = Vec::new();
-                let mut stream = rustls::Stream::new(&mut tls_session, &mut socket);
-                let read_bytes = stream.read(&mut buf);
-
-                log!(Level::Info, "read_bytes {:?}", read_bytes);
-
-                let request = String::from_utf8_lossy(&buf);
-                log!(Level::Info, "request {:?}", request);
-
-                let _ = stream.write_all(b"20 text/gemini\r\n#Hello\r\n");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let acceptor = acceptor.clone();
+                thread::spawn(move || {
+                    let mut stream = acceptor.accept(stream).unwrap();
+                    handle_client(&mut stream);
+                });
             }
-
-            _ => {}
+            Err(e) => {
+                warn!("{}", e.to_string())
+            }
         }
     }
 }
